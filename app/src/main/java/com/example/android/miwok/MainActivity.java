985fddf7787d46857c95bb6f44be2d956a0fb172
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2016-10-29 19:03:51 +0800

    Added logging for when activity openNumbersActivity is called
     On branch master
     Changes to be committed:
    	modified:   app/src/main/java/com/example/android/miwok/MainActivity.java

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,5 +34,6 @@ public class MainActivity extends AppCompatActivity {
     public void openNumbersActivity (View view) {
         Intent intent = new Intent(this, NumbersActivity.class);
         startActivity(intent);
+        Log.i("MainActivity", "OpenNumbersActivity - calling startActivity(NumbersActivity)");
     }
 }
