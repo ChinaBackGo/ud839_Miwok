f7f68f228a77133bc60cf0e62a03b835c8147971
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2016-11-05 20:03:40 +0800

    LESSON2: Quiz Add Text Views to the layout Skipped ahead a bit, and added the views using a loop. NumbersActivity.java: - Added the rootView variable as the linear layout for NumbersActivity - added TextViews to rootView - set text from words ArrayList activity_numbers.xml: - changed RelativeLayout for NumbersActivity to LinearLayout

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -29,16 +31,25 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(9, "nine");
         words.add(10, "ten");
 
+
+        LinearLayout rootView =  (LinearLayout)findViewById(R.id.activity_numbers);
+
         // Loop the ArrayList using a Java Iterator
         // TODO: How to get the iterator index?
+        // TODO: how to use foreach here?
         Iterator<String> wordsIterator = words.iterator();
-        int index = 0;
         while(wordsIterator.hasNext()) {
-            String word = wordsIterator.next();
-            Log.i("NumbersActivity.java", "Word at Index:" + index + " " + word);
-            index++;
+            //Create a new TextView, set the text to word
+            TextView childView = new TextView(this);
+            childView.setText(wordsIterator.next());
+
+            //Add the new wordTextView to parent numbersView
+            rootView.addView(childView);
+
+        //    Log.i("NumbersActivity.java", "Word at Index:" + index + " " + word);
+        //    index++;
         }
-        
+
         /*
         // Loop the Array List using a for loop
         for (int i = 0; i < words.size(); i++) {
