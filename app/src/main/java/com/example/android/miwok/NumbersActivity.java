6868eec568e7eaa0c5f02141cee0c63cbd561703
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2016-11-05 18:24:55 +0800

    Quiz: Add words to NumberActivity - added an array of strings for numbers activity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -9,5 +9,19 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String[] words = new String[11];
+        words[0] = "zero";
+        words[1] = "one";
+        words[2] = "two";
+        words[3] = "three";
+        words[4] = "four";
+        words[5] = "five";
+        words[6] = "six";
+        words[7] = "seven";
+        words[8] = "eight";
+        words[9] = "nine";
+        words[10] = "ten";
+
     }
 }
