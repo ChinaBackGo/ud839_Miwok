21fa2fbdc6c350bcae7f1ea82e26226147289498
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2016-11-05 19:26:25 +0800

    Lesson 2: Quiz Switch From Array to ArrayList switched words array to ArrayList added an iterator to loop the list and print log messages  Changes to be committed: 	modified:   app/src/main/java/com/example/android/miwok/NumbersActivity.java

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,18 +14,37 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words = new String[11];
-        words[0] = "zero";
-        words[1] = "one";
-        words[2] = "two";
-        words[3] = "three";
-        words[4] = "four";
-        words[5] = "five";
-        words[6] = "six";
-        words[7] = "seven";
-        words[8] = "eight";
-        words[9] = "nine";
-        words[10] = "ten";
+        //String[] words = new String[11];
+        ArrayList<String> words = new ArrayList<String>();
+
+        words.add(0, "zero");
+        words.add(1, "one");
+        words.add(2, "two");
+        words.add(3, "three");
+        words.add(4, "four");
+        words.add(5, "five");
+        words.add(6, "six");
+        words.add(7, "seven");
+        words.add(8, "eight");
+        words.add(9, "nine");
+        words.add(10, "ten");
+
+        // Loop the ArrayList using a Java Iterator
+        // TODO: How to get the iterator index?
+        Iterator<String> wordsIterator = words.iterator();
+        int index = 0;
+        while(wordsIterator.hasNext()) {
+            String word = wordsIterator.next();
+            Log.i("NumbersActivity.java", "Word at Index:" + index + " " + word);
+            index++;
+        }
+        
+        /*
+        // Loop the Array List using a for loop
+        for (int i = 0; i < words.size(); i++) {
+            Log.i("NumbersActivity.java", "Word at Index:" + i + " " + words.get(i));
+        }
+        */
 
     }
 }
