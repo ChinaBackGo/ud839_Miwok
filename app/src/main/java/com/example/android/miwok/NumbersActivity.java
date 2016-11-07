a68c00e183691311cb97bfc837cb41d33176538b
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2016-11-07 20:45:27 +0800

    Lesson 2: Quiz List View and ArrayAdapter - Switched to using a listView and arrayAdapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,9 +3,12 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -30,10 +33,33 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(8, "eight");
         words.add(9, "nine");
         words.add(10, "ten");
+        words.add(11, "eleven");
+        words.add(12, "twelve");
+        words.add(13, "thirteen");
+        words.add(14, "fourteen");
 
 
-        LinearLayout rootView =  (LinearLayout)findViewById(R.id.activity_numbers);
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemsAdapter);
+
+        //LinearLayout rootView =  (LinearLayout)findViewById(R.id.activity_numbers);
+        /*
         // Loop the ArrayList using a Java Iterator
         // TODO: How to get the iterator index?
         // TODO: how to use foreach here?
@@ -49,6 +75,7 @@ public class NumbersActivity extends AppCompatActivity {
         //    Log.i("NumbersActivity.java", "Word at Index:" + index + " " + word);
         //    index++;
         }
+        */
 
         /*
         // Loop the Array List using a for loop
