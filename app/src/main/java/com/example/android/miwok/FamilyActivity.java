b59a05ef61e2357e42e0f8b63ce5b0eebcb1daef
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2016-11-15 14:22:36 +0800

    Quiz: Remaining Words ColorsActivity.java FamilyActivity.java NumbersActivity.java PhrasesActivity.java
    Added words, arrayadapter, listview code to each Activity.
    Removed orphaned xml files (no longer needed as they all use word_list.xml
    renamed activity numbers.xml to word_list.xml
     On branch lesson2
     Changes to be committed:
    	modified:   app/src/main/java/com/example/android/miwok/ColorsActivity.java
    	modified:   app/src/main/java/com/example/android/miwok/FamilyActivity.java
    	modified:   app/src/main/java/com/example/android/miwok/NumbersActivity.java
    	modified:   app/src/main/java/com/example/android/miwok/PhrasesActivity.java
    	deleted:    app/src/main/res/layout/activity_colors.xml
    	deleted:    app/src/main/res/layout/activity_family.xml
    	deleted:    app/src/main/res/layout/activity_phrases.xml
    	renamed:    app/src/main/res/layout/activity_numbers.xml -> app/src/main/res/layout/word_list.xml

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,12 +2,50 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.word_list);
+
+        //String[] words = new String[11];
+        ArrayList<Word> words = new ArrayList<Word>();
+
+
+        //Add word object to words arrayList
+        words.add(new Word("father", "әpә"));
+        words.add(new Word("mother", "әṭa"));
+        words.add(new Word("son", "angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("youngest brother", "chalitti"));
+        words.add(new Word("oldest sister", "teṭe"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+
+
+        // Create an {@link WordAdapter}, whose data source is a list of Word objects. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // getView overrided method.
+        // This list item layout two text views and an image resource
+        WordAdapter adapter =
+                new WordAdapter(this, words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_list.xml file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,29 +2,22 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         //String[] words = new String[11];
         ArrayList<Word> words = new ArrayList<Word>();
 
 
         //Add word object to words arrayList
-        words.add(new Word("zero", "dingle"));
         words.add(new Word("one", "lutti"));
         words.add(new Word("two", "otiiko"));
         words.add(new Word("three", "tolookosu"));
@@ -35,8 +28,6 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word("eight", "kawinta"));
         words.add(new Word("nine", "wo'e"));
         words.add(new Word("ten", "na'aacha"));
-        words.add(new Word("dig", "dig"));
-        words.add(new Word("dug", "dug"));
 
 
         // Create an {@link WordAdapter}, whose data source is a list of Word objects. The
@@ -48,7 +39,7 @@ public class NumbersActivity extends AppCompatActivity {
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // activity_numbers.xml layout file.
+        // word_list.xml file.
         ListView listView = (ListView) findViewById(R.id.list);
 
         // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
