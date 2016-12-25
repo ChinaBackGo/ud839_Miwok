7f4da4451ea51b4141d5cd62a2ab4d3a040f4f27
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2016-12-25 09:30:18 -0800

    Lesson 5         Quiz Refactor Category Activities         applied refactor steps in 87e0385cbba999c77c5c07e66768e7e4a73812c7 to phrasesActivity to use PhrasesFragment *Activity.java Cleaned up unused import statements

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -1,191 +1,19 @@
 package com.example.android.miwok;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
-import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
-    //Logging TAG
-    private static final String TAG = "PhrasesActivity";
-
-    /** Handles playback of all the sound files */
-    private MediaPlayer mMediaPlayer;
-
-    /**
-     * Handles audio focus
-     */
-    private AudioManager mAudioManager;
-
-    /**
-     * This listener gets triggered when the {@link MediaPlayer} has completed
-     * playing the audio file.
-     */
-    private final MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer) {
-            // Now that the sound file has finished playing, release the media player resources.
-            releaseMediaPlayer();
-        }
-    };
-
-    /**
-     * This listener gets triggered when the {@link MediaPlayer} has an Error
-     */
-    private final MediaPlayer.OnErrorListener mOnErrorListener = new MediaPlayer.OnErrorListener() {
-        @Override
-        public boolean onError(MediaPlayer mp, int what, int extra) {
-            Log.e(TAG, "MediaPlayer Error: what: " + what + "extra: " + extra);
-            return false;
-        }
-    };
-
-    /**
-     * This listener gets triggered when the {@link AudioManager} focus changes
-     */
-    private final AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener =
-            new AudioManager.OnAudioFocusChangeListener() {
-                @Override
-                public void onAudioFocusChange(int focusChange) {
-                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
-                            focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-                        // Pause playback, seek to beginning
-                        mMediaPlayer.pause();
-                        mMediaPlayer.seekTo(0);
-                    } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-                        // Resume playback
-                        mMediaPlayer.start();
-                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-                        Log.i(TAG, "Abandon Audio Focus");
-                        // Stop playback
-                        releaseMediaPlayer();
-                    }
-                }
-            };
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.v(TAG, "onStop");
-        releaseMediaPlayer();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new PhrasesFragment())
+                .commit();
 
-        //initialize the Audio Manager
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-
-        //String[] words = new String[11];
-        ArrayList<Word> words = new ArrayList<>();
-
-
-        //Add word object to words arrayList
-        words.add(new Word("Where are you going?", "minto wuksus",
-                R.raw.phrase_where_are_you_going));
-        words.add(new Word("What is your name?", "tinnә oyaase'nә",
-                R.raw.phrase_what_is_your_name ));
-        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
-        words.add(new Word("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling));
-        words.add(new Word("I’m feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
-        words.add(new Word("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming));
-        words.add(new Word("Yes, I’m coming.", "hәә’ әәnәm", R.raw.phrase_yes_im_coming));
-        words.add(new Word("I’m coming.", "әәnәm", R.raw.phrase_im_coming));
-        words.add(new Word("Let’s go.", "yoowutis", R.raw.phrase_lets_go));
-        words.add(new Word("Come here.", "әnni'nem", R.raw.phrase_come_here));
-
-
-        // Create an {@link WordAdapter}, whose data source is a list of Word objects. The
-        // adapter knows how to create layouts for each item in the list, using the
-        // getView overrided method.
-        // This list item layout two text views and an image resource
-        WordAdapter adapter =
-                new WordAdapter(this, words, R.color.category_phrases);
-
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml file.
-        ListView listView = (ListView) findViewById(R.id.list);
-
-        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
-        // {@link ListView} will display list items for each word in the list of words.
-        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
-        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
-        listView.setAdapter(adapter);
-
-        //Register onClick listerner to start audio playback of word
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
-                Word currentWord = (Word) parent.getItemAtPosition(position);
-                currentWord.getRawResourceId();
-
-                releaseMediaPlayer();
-
-                // Request audio focus for playback
-                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
-                        // Use the music stream.
-                        AudioManager.STREAM_MUSIC,
-                        // Request transient focus.
-                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    //mAudioManager.registerMediaButtonEventReceiver(RemoteControlReceiver);
-                    // Start playback after we've been granted audio focus
-                    Log.i(TAG, "Audio Focus Granted");
-                    startMediaPlayer(view.getContext(), currentWord.getRawResourceId());
-                } else {
-                    Log.i(TAG, "Audio Focus Denied");
-                }
-
-                Log.v(TAG, "onItemClick: " + currentWord.toString());
-            }
-        });
-
-
-    }
-
-    /**
-     * Clean up the media player by releasing its resources.
-     */
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a sound.
-        if (mMediaPlayer != null) {
-            // Abandon focus
-            Log.i(TAG, "Abandon Audio Focus");
-            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
-
-            // Regardless of the current state of the media player, release its resources
-            // because we no longer need it.
-            Log.i(TAG, "MediaPlayer Released: " + mMediaPlayer.toString());
-            mMediaPlayer.release();
-
-            // Set the media player back to null. For our code, we've decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment.
-            mMediaPlayer = null;
-        }
-    }
-
-    /**
-     * Start Media playback
-     */
-    private void startMediaPlayer(Context context, int rawResourceid) {
-        Log.i(TAG, "Start mediaplayer");
-        mMediaPlayer = MediaPlayer.create(context, rawResourceid);
-        mMediaPlayer.start();
-        mMediaPlayer.setOnCompletionListener(mCompletionListener);
-        mMediaPlayer.setOnErrorListener(mOnErrorListener);
     }
 }
